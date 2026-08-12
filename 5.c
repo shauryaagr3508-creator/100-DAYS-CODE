657774516dad54cd78a29ae3451ff4f204df24d0
Shaurya Agrawal <shaurya.agr3508@gmail.com>
2026-08-12 10:42:12 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,28 @@
+/*Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
+
+
+//Formulae for conversion: F = (9*C/5) + 32
+
+#include<stdio.h>
+int main(){
+
+printf("fahrenheit = %d\n" , (9*0/5) + 32);
+printf("fahrenheit = %d\n" , (9*100/5) + 32);
+return 0;
+
+
+}
