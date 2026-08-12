812797c4b28b4c937c50e79fbf811907062086f6
Shaurya Agrawal <shaurya.agr3508@gmail.com>
2026-08-12 09:23:53 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,24 @@
+/*Q1: Write a program to input two numbers and display their sum.
+
+
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+
+
+#include<stdio.h>
+int main(){
+printf("%d\n", 3+4);
+printf("%d\n", -1+20);
+return 0;
+
+}
